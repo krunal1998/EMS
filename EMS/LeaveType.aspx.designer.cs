52a9efe188301ae4934794a78bdba3e30e14857a
Krunal <katrodiya80@gmail.com>
2019-02-20 13:19:21 +0530

    Revert "Revert "leavetype""
    This reverts commit b1b38ffa8ebbfef64a87979d56e08856f0ba6d0c.

--- EMS/LeaveType.aspx.designer.cs
+++ EMS/LeaveType.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace EMS {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
-        /// PlaceHolder1 control.
+        /// CheckBoxList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder PlaceHolder1;
+        protected global::System.Web.UI.WebControls.CheckBoxList CheckBoxList1;
         
         /// <summary>
         /// DeleteButton control.
