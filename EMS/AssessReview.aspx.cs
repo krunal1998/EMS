a89a44db5a62e4b93f51b67b63b38cc393d1f4e5
Yash <changraniyash@gmail.com>
2019-02-22 13:51:39 +0530

    assess review and pending reviews implementation.

--- EMS/AssessReview.aspx.cs
+++ EMS/AssessReview.aspx.cs
@@ -4,14 +4,218 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace EMS
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class AssessReview : System.Web.UI.Page
     {
+        static List<PERFORMANCEPARAMETER> parameterList;
+        GENERATEREVIEW g;
         protected void Page_Load(object sender, EventArgs e)
         {
+            int grid = Convert.ToInt32(Session["GRID"]);
+            g=  getgeneratedreview(grid);
+            EMPLOYEE emp = getEmployee(g.EmployeeId);
 
+
+            String ename = getEmployeeName(emp);
+            FromLabel.Text = g.StartDate.ToString();
+            ToLabel.Text = g.EndDate.ToString();
+            NameLabel.Text = ename;
+
+            parameterList = getParameters(emp.JobtitleId.Value);
+
+            foreach (PERFORMANCEPARAMETER p in parameterList) {
+                TableRow row = new TableRow();
+                TableCell pname = new TableCell();
+                Label pnamelabel = new Label();
+                pnamelabel.Text = p.ParameterName;
+                pname.Controls.Add(pnamelabel);
+                row.Cells.Add(pname);
+
+                TableCell review = new TableCell();
+                for (int i = p.MinRating; i <= p.MaxRating; i++) {
+                    RadioButton r = new RadioButton();
+                    r.GroupName = p.ParameterName;
+                    r.Text = i.ToString();
+                    
+                    r.Width = 50;
+                    r.ID = p.ParameterName + i;
+                    
+                    if (i == (p.MinRating + p.MaxRating) / 2)
+                        r.Checked = true;
+                    else
+                        r.Checked = false;
+
+                    review.Controls.Add(r);
+                }
+                row.Cells.Add(review);
+
+                TableCell comment = new TableCell();
+                TextBox commenttb = new TextBox();
+                commenttb.ID = p.ParameterId + p.ParameterName;
+                comment.Controls.Add(commenttb);
+                row.Cells.Add(comment);
+
+                ParametersTable.Rows.Add(row);
+                pnamelabel.ID = ParametersTable.Rows.GetRowIndex(row).ToString();
+
+            }
+
+        }
+
+        private List<PERFORMANCEPARAMETER> getParameters(int jobtitleId)
+        {
+            List<PERFORMANCEPARAMETER> plist = new List<PERFORMANCEPARAMETER>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Global.URIstring);
+                //HTTP GET
+                var responseTask = client.GetAsync("Parameters/");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<PERFORMANCEPARAMETER[]>();
+                    readTask.Wait();
+
+                     var parameters= readTask.Result;
+
+                    foreach (var p in parameters)
+                    {
+                        
+                        if (jobtitleId == p.JobTitleId)
+                        {
+                            plist.Add(p);
+                        }
+                    }
+
+                }
+                return plist;
+            }
+
+        }
+
+        private EMPLOYEE getEmployee(string employeeID)
+        {
+            EMPLOYEE e;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Global.URIstring);
+                //HTTP GET
+                var responseTask = client.GetAsync("Employees/" + employeeID);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<EMPLOYEE>();
+                    readTask.Wait();
+
+                    e = readTask.Result;
+                    return e;
+
+                }
+                else return null;
+            }
+
+        }
+
+        private GENERATEREVIEW getgeneratedreview(int grid)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Global.URIstring);
+                //HTTP GET
+                var responseTask = client.GetAsync("GeneratedReviews/" + grid);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<GENERATEREVIEW>();
+                    readTask.Wait();
+
+                    GENERATEREVIEW g = readTask.Result;
+                    return g;
+                }
+                else return null;
+            }
+        }
+
+        private string getEmployeeName(EMPLOYEE e)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Global.URIstring);
+                //HTTP GET
+                var responseTask = client.GetAsync("PersonalDetails/" + e.PersonalDetailId.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<PERSONALDETAILS>();
+                    readTask.Wait();
+
+                    PERSONALDETAILS pd = readTask.Result;
+                    return pd.FirstName + " " + pd.LastName;
+                }
+                else return "";
+            }
+
+        }
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            foreach (TableRow tr in ParametersTable.Rows)
+            {
+                if (tr.Cells.Count == 3 && tr != ParametersTable.Rows[0])
+                {
+                    Label paramname = (Label)tr.Cells[0].FindControl(ParametersTable.Rows.GetRowIndex(tr).ToString());
+                    PERFORMANCEPARAMETER p = parameterList.FirstOrDefault(pr => pr.ParameterName == paramname.Text);
+                    REVIEW r = new REVIEW();
+                    r.GenerateReviewId = g.GenerateReviewId;
+                    r.EmployeeId = g.EmployeeId;
+                    r.PerameterId = p.ParameterId;
+                    TextBox comment = (TextBox)tr.Cells[2].FindControl(p.ParameterId + p.ParameterName);
+                    r.Comment = comment.Text;
+
+                    for (int i = p.MinRating; i <= p.MaxRating; i++)
+                    {
+                        RadioButton rb = (RadioButton)tr.Cells[1].FindControl(p.ParameterName + i);
+                        if (rb.Checked)
+                        {
+                            r.Rating = Convert.ToInt32(rb.Text);
+                            break;
+                        }
+                    }
+                    // Response.Write(r.GenerateReviewId + " " +r.PerameterId+" "+ r.Rating + " " + r.Comment + "\n" );
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri(Global.URIstring);
+
+                    var postTask = client.PostAsJsonAsync<REVIEW>("reviews", r);
+                    postTask.Wait();
+
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        Response.Write("Save successful!");
+
+
+
+                    }
+                }
+            }
         }
     }
 }
--- EMS/AssessReview.aspx.designer.cs
+++ EMS/AssessReview.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace EMS {
     
     
-    public partial class WebForm2 {
+    public partial class AssessReview {
         
         /// <summary>
         /// NameLabel control.
@@ -40,165 +40,21 @@ namespace EMS {
         protected global::System.Web.UI.WebControls.Label ToLabel;
         
         /// <summary>
-        /// ParameterLabel1 control.
+        /// ParametersTable control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label ParameterLabel1;
+        protected global::System.Web.UI.WebControls.Table ParametersTable;
         
         /// <summary>
-        /// RadioButton1 control.
+        /// Save control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton1;
-        
-        /// <summary>
-        /// RadioButton2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton2;
-        
-        /// <summary>
-        /// RadioButton3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton3;
-        
-        /// <summary>
-        /// RadioButton4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton4;
-        
-        /// <summary>
-        /// RadioButton5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton5;
-        
-        /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// RadioButton6 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton6;
-        
-        /// <summary>
-        /// RadioButton7 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton7;
-        
-        /// <summary>
-        /// RadioButton8 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton8;
-        
-        /// <summary>
-        /// RadioButton9 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton9;
-        
-        /// <summary>
-        /// RadioButton10 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton10;
-        
-        /// <summary>
-        /// RadioButton11 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton11;
-        
-        /// <summary>
-        /// RadioButton12 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton12;
-        
-        /// <summary>
-        /// RadioButton13 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton13;
-        
-        /// <summary>
-        /// RadioButton14 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton14;
-        
-        /// <summary>
-        /// RadioButton15 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButton RadioButton15;
-        
-        /// <summary>
-        /// Add control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Add;
+        protected global::System.Web.UI.WebControls.Button Save;
     }
 }
